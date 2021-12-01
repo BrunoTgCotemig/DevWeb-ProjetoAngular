126b975a3febeba77136140201784b3d3e10970b
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-12-01 19:45:13 -0300

    [T7] -> adddata is now properly loading

--- main-FrontEnd/frontend/src/app/app.module.ts
+++ main-FrontEnd/frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatSidenavModule } from "@angular/material/sidenav"
 import { MatListModule } from "@angular/material/list"
 import { MatCardModule } from "@angular/material/card"
 import { MatButtonModule } from "@angular/material/button"
+import { MatSnackBarModule } from "@angular/material/snack-bar"
 
 
 import { FooterComponent } from './components/OnScreenElements/footer/footer.component';
@@ -41,7 +42,8 @@ import { ViewAdddataTransitionComponent } from './components/PageViews/view-addd
     MatListModule,
     MatSidenavModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- main-FrontEnd/frontend/src/app/components/PageViews/view-adddata/view-adddata.component.ts
+++ main-FrontEnd/frontend/src/app/components/PageViews/view-adddata/view-adddata.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router"
+import { SManageDataService } from "../../../Services_and_Related/s-manage-data.service"
+
+
 
 @Component({
   selector: 'app-view-adddata',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-adddata.component.css']
 })
 export class ViewAdddataComponent implements OnInit {
+  constructor(private smanageservice: SManageDataService, private router:Router) {}
 
-  constructor() { }
+
 
   ngOnInit(): void {
+
+    
   }
+
+
+  DB_adddata(){
+    
+    this.smanageservice.GenerateMessage("Registro completo!")
+  }
+
+
+  GoTo_trnstn():void{   this.router.navigate(["/trnstn"])   }
+
+
 
 }
