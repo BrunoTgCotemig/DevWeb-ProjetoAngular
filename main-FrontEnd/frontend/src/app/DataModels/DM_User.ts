cca402d90afcf64554b193996eda8f4c471a9090
Bruno T. Galvao <11900989@aluno.cotemig.com.br>
2021-11-30 21:11:13 -0300

    Addendum -1

---
+++ main-FrontEnd/frontend/src/app/DataModels/DM_User.ts
@@ -0,0 +1,11 @@
+export interface User {
+
+    Name: string;
+    Age: number;
+    Gender: string;
+    Email: string;
+    PhoneNumber: string;
+    CountryOfOrigin: string;
+    CityOfOrigin: string;
+    
+}
